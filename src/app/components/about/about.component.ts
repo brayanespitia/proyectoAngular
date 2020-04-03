4534b522d132c138341b3a6d7267ac1a42e7f802
brayan <brayanespitia2013@gmail.com>
2020-04-03 11:52:15 -0500

    maquetando el about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public web: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.title = "Brayan Espitia";
+    this.subtitle = "Estudiante de ing de sistemas";
+    this.web = "facebook.com";
   }
 
+  ngOnInit(): void {}
 }
