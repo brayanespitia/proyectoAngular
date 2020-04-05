cc7d6a1fd1a3ea3a6be2cd0ee75e84f0fea8b1f6
brayan <brayanespitia2013@gmail.com>
2020-04-04 20:06:15 -0500

    reiniciando formulario

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -11,6 +11,8 @@ import { ProjectService } from "../../services/project.service";
 export class CreateComponent implements OnInit {
   public title: string;
   public project: Project;
+  public status: string;
+
   constructor(private _projectService: ProjectService) {
     this.title = "crear proyecto";
     this.project = new Project("", "", "", "", 2020, "", "");
@@ -22,7 +24,12 @@ export class CreateComponent implements OnInit {
     console.log(this.project);
     this._projectService.saveProject(this.project).subscribe(
       (response) => {
-        console.log(response);
+        if (response.project) {
+          this.status = "success";
+          form.reset();
+        } else {
+          this.status = "failed";
+        }
       },
       (error) => {
         console.log(<any>error);
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -19,9 +19,8 @@ export class ProjectService {
   saveProject(project: Project): Observable<any> {
     let params = JSON.stringify(project);
     let headers = new HttpHeaders().set("Content-Type", "application/json");
-    return this._http.post(this.url+"save-project", params, {
-      headers: headers
+    return this._http.post(this.url + "save-project", params, {
+      headers: headers,
     });
-    console.log("aqui esta el error");
   }
 }
